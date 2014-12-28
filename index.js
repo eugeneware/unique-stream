5c26c2da5e01d959c0b9f371c3fdecffde94f6b0
Craig Ambrose <craig@craigambrose.com>
2014-12-28 21:40:54 +1300

    Refactored to use through2-filter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Stream = require('stream');
+var filter = require("through2-filter").obj
 
 function prop(propName) {
   return function (data) {
@@ -14,41 +14,16 @@ function unique(propName) {
   } else if (typeof propName === 'function') {
     keyfn = propName;
   }
-  var seen = {};
-  var s = new Stream();
-  s.readable = true;
-  s.writable = true;
-  var pipes = 0;
 
-  s.write = function (data) {
+  var seen = {};
+
+  return filter(function (data) {
     var key = keyfn(data);
     if (seen[key] === undefined) {
       seen[key] = true;
-      s.emit('data', data);
+      return true
+    } else {
+      return false
     }
-  };
-
-  var ended = 0;
-  s.end = function (data) {
-    if (arguments.length) s.write(data);
-    ended++;
-    if (ended === pipes || pipes === 0) {
-      s.writable = false;
-      s.emit('end');
-    }
-  };
-
-  s.destroy = function (data) {
-    s.writable = false;
-  };
-
-  s.on('pipe', function () {
-    pipes++;
   });
-
-  s.on('unpipe', function () {
-    pipes--;
-  });
-
-  return s;
 }
